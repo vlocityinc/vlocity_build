25fb0f210931fa9dbdc0a6702e23cb48b7565b51
Adam Rutland <arutland@vlocity.com>
2018-06-04 13:54:26 -0700

    Add test class for datapacksutils.

---
+++ test/datapacksutils.spec.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const _datapacksutils = require('../lib/datapacksutils');
+const expect = require('chai').expect;
+
+describe('DataPacksUtils', () => 
+{
+    var datapacksutils = new _datapacksutils();
+    
+    it('should not be null', () => { 
+        expect(_datapacksutils).to.not.be.eq(null);
+        expect(datapacksutils).to.not.be.eq(null)
+    }) 
+    
+    describe('guid', () => {
+        it('should be a function', () => {
+            expect(datapacksutils.guid).to.be.a('function')
+        })
+
+        var generatedGuids = {};
+
+        for (var i = 0; i < 1000000; i++) {
+            var newGuid = datapacksutils.guid();
+
+            expect(generatedGuids[newGuid]).to.be.eq(undefined);
+
+            generatedGuids[newGuid] = newGuid;
+        }
+    });
+})
