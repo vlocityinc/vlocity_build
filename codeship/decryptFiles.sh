52f8b21f4b36d66a87530cec56d5dee8707fc00a
Adam Rutland <arutland@vlocity.com>
2018-09-23 13:32:39 -0700

    Remove statement is script

--- codeship/decryptFiles.sh
+++ codeship/decryptFiles.sh
@@ -5,6 +5,4 @@ for filename in codeship/encrypted_files/*; do
     BASE=`echo "$(basename $filename)"`
 
     openssl enc -d -aes-256-cbc -pass pass:$AES_KEY -in codeship/encrypted_files/$BASE -out codeship/unencrypted_files/$BASE
-
-    cat codeship/unencrypted_files/$BASE
 done
