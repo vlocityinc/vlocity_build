3f5095da71327504750ce8c51236743f41871003
Adam Rutland <arutland@vlocity.com>
2018-10-06 05:32:55 -0700

    Fix RuleAssignment issue in Product Deploy. Make Documents undiffable. Add build manifest - work in progress

--- codeship/publish.sh
+++ codeship/publish.sh
@@ -5,11 +5,11 @@ set -e
 
 cp codeship/unencrypted_files/npmrc .npmrc
 
-#if [ $CI_BRANCH == "master" ]; then
-#    npm publish .
-#else 
-#    npm publish . --tag "next"
-#fi
+if [ $CI_BRANCH == "master" ]; then
+    npm publish .
+else 
+    npm publish . --tag="next"
+fi
 
 npm run-script build
 
@@ -18,7 +18,7 @@ P_VERSION=`cat package.json | jq -r '. | .version'`
 GITHUB_ASSETS="dist/vlocity-linux-x64,dist/vlocity-macos-x64,dist/vlocity-win-x64.exe,dist/vlocity-win-x86.exe"
 
 if [ $CI_BRANCH == "master" ]; then
-    publish-release --notes "$P_VERSION" --token $GITHUB --target_commitish $CI_BRANCH --owner vlocityinc --repo vlocity_build --name "v$P_VERSION" --tag "v$P_VERSION" --assets $GITHUB_ASSETS --draft
+    publish-release --notes "$P_VERSION" --token $GITHUB --target_commitish $CI_BRANCH --owner vlocityinc --repo vlocity_build --name "v$P_VERSION" --tag "v$P_VERSION" --assets "$GITHUB_ASSETS" --draft
 else 
-    publish-release --notes "Beta - v$P_VERSION" --token $GITHUB --target_commitish $CI_BRANCH --prerelease --reuseRelease --owner vlocityinc --repo vlocity_build --name "Beta-v$P_VERSION" --editRelease --tag next --assets $GITHUB_ASSETS --draft
+    publish-release --notes "Beta - v$P_VERSION" --token $GITHUB --target_commitish $CI_BRANCH --prerelease --reuseRelease --owner vlocityinc --repo vlocity_build --name "Beta-v$P_VERSION" --editRelease --tag next --assets "$GITHUB_ASSETS" --draft
 fi
