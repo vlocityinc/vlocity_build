534127482baadeedfac0e9a2bdd6804c31b3b213
Adam Rutland <arutland@vlocity.com>
2018-10-14 07:59:19 -0700

    Updates for better error handling. Show more consistent time.

--- javascript/removeStandardPricebokEntry.js
+++ javascript/removeStandardPricebokEntry.js
@@ -1,8 +1,4 @@
 module.exports = function(vlocity, dataPackData, jobInfo, callback) {
-
-    //console.log(dataPackData);
     delete dataPackData.VlocityDataPackData.Product2[0].PricebookEntry;
-
-   // console.log(JSON.stringify(dataPackData));
     callback();
 }
