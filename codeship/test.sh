6fe87ec4bffe4888159cd3a7c4edb157ffee9ac2
Adam Rutland <arutland@vlocity.com>
2018-10-14 16:02:38 -0700

    Fix binary issues. Add more tests for binaries.

--- codeship/test.sh
+++ codeship/test.sh
@@ -13,4 +13,14 @@ npm link
 vlocity -sfdx.username $SF_USERNAME runTestJob
 
 # Must return a JSON with a result
-vlocity -sfdx.username $SF_USERNAME runTestJob --json | jq .status
+vlocity -sfdx.username $SF_USERNAME runTestJob --json | jq .
+
+npm run-script build
+
+./dist/vlocity-linux-x64 -sfdx.username $SF_USERNAME --nojob packExport -key VlocityCard/datapacktest-card
+
+./dist/vlocity-linux-x64 -sfdx.username $SF_USERNAME --nojob packGetAllAvailableExports --json | jq .
+
+./dist/vlocity-linux-x64 -sfdx.username $SF_USERNAME -projectPath "vlocity-temp" --nojob packExport -key VlocityCard/datapacktest-card
+
+./dist/vlocity-linux-x64 -sfdx.username $SF_USERNAME -projectPath "vlocity-temp" --nojob packDeploy -key VlocityCard/datapacktest-card --json | jq .
