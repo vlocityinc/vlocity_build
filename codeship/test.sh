cb2d3c240e294646cac277117845e4a237746185
mdmehran-qureshi <mdmehran.qureshi@salesforce.com>
2025-01-24 14:40:04 +0530

    Org Setup for CodeShip

--- codeship/test.sh
+++ codeship/test.sh
@@ -5,10 +5,9 @@ set -e
 
 sf
 
-#SF_AUTH_ORG=`sf org login sfdx-url -f codeship/unencrypted_files/test.sfdx --json`
-#SF_USERNAME=`echo $SF_AUTH_ORG | jq -r '. | .result.username'`
+SF_AUTH_ORG=`sf org login sfdx-url --sfdx-url-file codeship/unencrypted_files/test.json --json`
+SF_USERNAME=`echo $SF_AUTH_ORG | jq -r '. | .result.username'`
 
-SF_USERNAME=`echo testvbt_build@vbttest.com` 
 
 sf alias set VB_TEST_ORG=$SF_USERNAME
 
@@ -16,12 +15,12 @@ npm run-script unitTest
 
 npm link
 
-vlocity -sfdx.username $SF_USERNAME -authFilePath codeship/unencrypted_files/test.sfdx runTestJob --verbose
+vlocity -sfdx.username $SF_USERNAME runTestJob --verbose
 
 echo 'Running JSON Jobs - Takes up to 10 minutes with no output'
 
 # Must return a JSON with a result
-vlocity -sfdx.username $SF_USERNAME -authFilePath codeship/unencrypted_files/test.sfdx runTestJob --json | jq .
+vlocity -sfdx.username $SF_USERNAME runTestJob --json | jq .
 
 
 #npm run-script build
